e78dcdf21b65bc75d76b4d4005d9516551a82a0d
momo5502 <mail@momo5502.com>
2019-01-13 12:07:19 +0100

    Experimental chaiscript

--- src/module/scripting.cpp
+++ src/module/scripting.cpp
@@ -7,8 +7,20 @@ class scripting final : public module
 public:
 	void post_load() override
 	{
-		
+		this->chai_.add(chaiscript::fun(&function), "function");
+
+		double d = this->chai_.eval<double>("function(3, 4.75);");
+		printf("Result: %f", d);
 	}
+
+private:
+	chaiscript::ChaiScript chai_;
+
+	static double function(int i, double j)
+	{
+		return i * j;
+	}
+
 };
 
 REGISTER_MODULE(scripting)
